7e33d63e689a62bf392d9f6c4bb26b7ffd54a805
delano0161 <147700998+delano0161@users.noreply.github.com>
2024-03-06 11:42:04 +0100

    opdracht van de les af

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -10,6 +10,18 @@ echo $attractie . " / " . $capaciteit . " / " . $melder;
 //1. Verbinding
 require_once '../../../config/conn.php';
 
+$query = "INSERT INTO meldingen (attractie, capaciteit, melder)
+VALUES (:attractie, :capaciteit, :melder)";
+
+$statement = $conn->prepare($query);
+
+$statement->execute([
+    ":attractie"=>$attractie,
+    ":capaciteit"=>$capaciteit,
+    ":melder"=>$melder,
+]);
+
+$items = $statement->fetchAll(PDO::FETCH_ASSOC);
 //2. Query
 
 //3. Prepare
